fe0c5e56d574f7494a4cb625495fd29faf9542c1
KarZan-iT <35143316+KarZan-iT@users.noreply.github.com>
2018-02-01 22:32:01 +0300

    Add Kurdish language
    Add Arabic language

---
+++ translations/stacer_krd.ts
@@ -0,0 +1,454 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="Kurdish_Krd">
+<context>
+    <name>App</name>
+    <message>
+        <location filename="../stacer/app.cpp" line="80"/>
+        <source>Dashboard</source>
+        <translation>تەختەپێوانەکان - سەرەتا</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="85"/>
+        <source>System Cleaner</source>
+        <translation>خاوێنکەرەوەی سیستەم</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="90"/>
+        <source>System Startup Apps</source>
+        <translation>هەڵبوونی نەرمەواڵاکان لەگەڵ دەسپێکی سیستەم</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="95"/>
+        <source>System Services</source>
+        <translation>خزمەتگوزاری سیستەم</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="100"/>
+        <source>Uninstaller</source>
+        <translation>هەڵوەشاندنەوە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="105"/>
+        <source>Resources</source>
+        <translation>سەرچاوەکان</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="110"/>
+        <source>Processes</source>
+        <translation>کردارەکان</translation>
+    </message>
+    <message>
+        <location filename="../stacer/app.cpp" line="115"/>
+        <source>Settings</source>
+        <translation>ڕێکخستنەکان</translation>
+    </message>
+</context>
+<context>
+    <name>DashboardPage</name>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.ui" line="153"/>
+        <source>SYSTEM INFO</source>
+        <translation>زانیاری سیستەم</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.ui" line="240"/>
+        <source>There are update currently available.</source>
+        <translation>ئێستا نوێکردنەوە بەردەستە.</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.ui" line="253"/>
+        <source>Download</source>
+        <translation>داگرتن</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="18"/>
+        <source>CPU</source>
+        <translation>ڕێکخەری ناوەندی</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="19"/>
+        <source>MEMORY</source>
+        <translation>بیرگە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="20"/>
+        <source>DISK</source>
+        <translation>پەپکە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="21"/>
+        <source>DOWNLOAD</source>
+        <translation>داگرتن</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="22"/>
+        <source>UPLOAD</source>
+        <translation>بارکردن</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="107"/>
+        <source>Hostname: %1</source>
+        <translation>ناوی خانەخوێ: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="108"/>
+        <source>Platform: %1</source>
+        <translation>سەکۆ: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="109"/>
+        <source>Distribution: %1</source>
+        <translation>دابەشکراوەی: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="110"/>
+        <source>Kernel Release: %1</source>
+        <translation>وەشانی ناوک: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="111"/>
+        <source>CPU Model: %1</source>
+        <translation>نموونەی ڕێکخەری ناوەندی: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="112"/>
+        <source>CPU Speed: %1</source>
+        <translation>خێرای ڕێکخەری ناوەندی: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="113"/>
+        <source>CPU Core: %1</source>
+        <translation>ناوکی ڕێکخەری ناوەندی: %1</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="186"/>
+        <location filename="../stacer/Pages/Dashboard/dashboard_page.cpp" line="190"/>
+        <source>Total: %1</source>
+        <translation>سەرجەم: %1</translation>
+    </message>
+</context>
+<context>
+    <name>ProcessesPage</name>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.ui" line="55"/>
+        <source>Processes</source>
+        <translation>کردارەکان</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.ui" line="68"/>
+        <source>All Processes</source>
+        <translation>هەموو کردارەکان</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.ui" line="88"/>
+        <source>Search...</source>
+        <translation>گەڕان...</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.ui" line="206"/>
+        <source>End Process</source>
+        <translation>کۆتایی کردارەکە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="35"/>
+        <source>Resident Memory</source>
+        <translation>بەڕێوبەری بیرگە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="35"/>
+        <source>%Memory</source>
+        <translation>%بیرگە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="35"/>
+        <source>Virtual Memory</source>
+        <translation>گریمانەی بیرگە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="35"/>
+        <source>User</source>
+        <translation>بەکارهێنەر</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>Start Time</source>
+        <translation>دەستپێکردنی کات</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>State</source>
+        <translation>دۆخی</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>Group</source>
+        <translation>دەستەی</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>Nice</source>
+        <translation>Nice</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="36"/>
+        <source>CPU Time</source>
+        <translation>کاتی ڕێکخەری ناوەکی</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="37"/>
+        <source>Session</source>
+        <translation>شوێن</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="37"/>
+        <source>Seat</source>
+        <translation>جێگا</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="37"/>
+        <source>Process</source>
+        <translation>کردار</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="104"/>
+        <source>Processes (%1)</source>
+        <translation>کردارەکان (%1)</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Processes/processes_page.cpp" line="198"/>
+        <source>Refresh (%1)</source>
+        <translation>بوژاندنەوە (%1)</translation>
+    </message>
+</context>
+<context>
+    <name>ResourcesPage</name>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="26"/>
+        <source>CPU History</source>
+        <translation>مێژووی ڕێکخەری ناوەکی</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="29"/>
+        <source>Memory History</source>
+        <translation>مێژووی بیرگە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="32"/>
+        <source>Network History</source>
+        <translation>مێژووی ڕایەڵە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="76"/>
+        <source>Download %1/s Total: %2</source>
+        <translation>داگرتن %1/چرکە سەرجەم: %2</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="81"/>
+        <source>Upload %1/s  Total: %2</source>
+        <translation>باکردنی %1/چرکە  سەرجەم: %2</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="122"/>
+        <source>Swap %1 (%2%) %3</source>
+        <translation>جێگرەوە %1 (%2%) %3</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Resources/resources_page.cpp" line="131"/>
+        <source>Memory %1 (%2%) %3</source>
+        <translation>بیرگە %1 (%2%) %3</translation>
+    </message>
+</context>
+<context>
+    <name>ServicesPage</name>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.ui" line="79"/>
+        <source>System Services</source>
+        <translation>خزمەتگوزاری سیستەم</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.ui" line="123"/>
+        <source>Startup at boot ?</source>
+        <translation>هەڵبوون و خستنەڕیی بووت ؟</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.ui" line="176"/>
+        <source>Running Now ?</source>
+        <translation>کارپێکردن هەر ئێستا ؟</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.ui" line="246"/>
+        <source>Not Found System Service</source>
+        <translation>ئەم خزمەتگوزاریە بەردەست نییە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Services/services_page.cpp" line="50"/>
+        <source>System Services (%1)</source>
+        <translation>خزمەتگوزاری سیستەم (%1)</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../stacer/Pages/Settings/settings_page.ui" line="54"/>
+        <source>Language</source>
+        <translation>زمان</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Settings/settings_page.ui" line="71"/>
+        <source>Theme</source>
+        <translation>ڕوکار</translation>
+    </message>
+</context>
+<context>
+    <name>StartupApp</name>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app.cpp" line="23"/>
+        <source>Delete</source>
+        <translation>سڕینەوە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app.cpp" line="24"/>
+        <source>Edit</source>
+        <translation>دەستکاری</translation>
+    </message>
+</context>
+<context>
+    <name>StartupAppEdit</name>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="20"/>
+        <source>Startup App</source>
+        <translation>پڕۆگرامەکانی پێش هەڵبوون</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="50"/>
+        <source>Save</source>
+        <translation>پاشەکەوتکردن</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="60"/>
+        <source>Fields cannot be left blank. </source>
+        <translation>بوار نییە ئێرە بە بەتاڵی جێبهێڵیت. </translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="67"/>
+        <source>App Comment</source>
+        <translation>لێدوانی نەرمەواڵا</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="74"/>
+        <source>App Name</source>
+        <translation>ناوی نەرمەواڵا</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="81"/>
+        <source>Command</source>
+        <translation>فرمان</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_app_edit.ui" line="88"/>
+        <source>Application</source>
+        <translation>نەرمەواڵە</translation>
+    </message>
+</context>
+<context>
+    <name>StartupAppsPage</name>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_apps_page.ui" line="95"/>
+        <source>Add Startup App</source>
+        <translation>زیادکردنی نەرمەواڵا</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_apps_page.ui" line="112"/>
+        <source>System Startup Applications</source>
+        <translation>هەڵبوونی نەرمەواڵاکان لەگەڵ دەسپێکی سیستەم</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_apps_page.ui" line="192"/>
+        <source>Not Found Startup Apps</source>
+        <translation>هیچ نەرمەواڵەیە بەردەست نییە لەکاتی هەڵبوونی سیستەم</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/StartupApps/startup_apps_page.cpp" line="79"/>
+        <source>System Startup Applications (%1)</source>
+        <translation>پڕۆگەامی بەردەست لە کاتی هەڵبوونی سیستەم (%1)</translation>
+    </message>
+</context>
+<context>
+    <name>SystemCleanerPage</name>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="63"/>
+        <source>Crash Reports</source>
+        <translation>ڕاپۆرتی تێکشکاوەکان</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="168"/>
+        <source>Application Logs</source>
+        <translation>تۆماری نەرمەواڵاکان</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="206"/>
+        <source>Application Caches</source>
+        <translation>نەرمەواڵەی هەڵگیراو</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="327"/>
+        <source>Trash</source>
+        <translation>تەنەکەی خۆڵ</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="365"/>
+        <source>Package Caches</source>
+        <translation>بوخچەی هەڵگیراو</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.ui" line="473"/>
+        <source> Back</source>
+        <translation> گەڕانەوە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.cpp" line="32"/>
+        <source>File Name</source>
+        <translation>ناوی پەڕگە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.cpp" line="32"/>
+        <source>Size</source>
+        <translation>قەبارە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/SystemCleaner/system_cleaner_page.cpp" line="285"/>
+        <source>%1 size files cleaned.</source>
+        <translation>%1 قەبارەی ئەوپەڕگانەی پاکرانەوە.</translation>
+    </message>
+</context>
+<context>
+    <name>UninstallerPage</name>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="132"/>
+        <source>Uninstall Selected</source>
+        <translation>هەڵوەشاندنەوەی هەڵبژێراو</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="178"/>
+        <source>Not Found Installed Packages</source>
+        <translation>هیچ گورزەیەکی دامەزارو نییە</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="228"/>
+        <source>System Installed Packages</source>
+        <translation>گورزەی دامەزارو لەگەڵ سیستەم</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstallerpage.ui" line="271"/>
+        <source>Search...</source>
+        <translation>گەڕان...</translation>
+    </message>
+    <message>
+        <location filename="../stacer/Pages/Uninstaller/uninstaller_page.cpp" line="65"/>
+        <source>System Installed Packages (%1)</source>
+        <translation>گورزەی دامەزارو لەگەڵ سیستەم (%1)</translation>
+    </message>
+</context>
+</TS>
+
